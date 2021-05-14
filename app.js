7f726741e40eba68c9ff32af200f8d70f68baf97
Luca de Acha <lucadeacha@gmail.com>
2021-05-14 18:54:19 -0300

    added missin character

--- app.js
+++ app.js
@@ -42,8 +42,9 @@ if(process.env.NODE_ENV=="production"){
     //so the react code can handle the requests
     app.use(express.static('client/build'))
     const path = require('path')
+    
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(_dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
 
